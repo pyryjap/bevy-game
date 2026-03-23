f5821e259ce4cec20acb6d8fac5ff2b333e1d53e
pyryjap <70763071+pyryjap@users.noreply.github.com>
2026-03-23 22:58:45 +0200

    Migrate to Bevy 0.18 from Bevy 0.10

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::prelude::*;
+use bevy::{
+    asset::RenderAssetUsages, mesh::Indices, prelude::*, render::render_resource::PrimitiveTopology,
+};
 
 const FLOOR_Y: f32 = -360.0;
 const PLAYER_RADIUS: f32 = 25.0;
@@ -10,36 +12,42 @@ fn main() {
 
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_plugin(PhysicsPlugin)
-        .add_systems((
-            player_impulse_system.before(integrator_before_system),
-            player_force_system
-                .after(integrator_before_system)
-                .before(integrator_after_system),
-        ))
-        .add_system(bevy::window::close_on_esc)
+        .add_plugins(PhysicsPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, close_on_esc)
         .run();
+}
+
+fn close_on_esc(
+    mut commands: Commands,
+    focused_windows: Query<(Entity, &Window)>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    for (window, _focus) in focused_windows.iter() {
+        if input.just_pressed(KeyCode::Escape) {
+            commands.entity(window).despawn();
+        }
+    }
 }
 
 struct PhysicsPlugin;
 
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems((
-            integrator_before_system,
-            gravity_system
-                .after(integrator_before_system)
-                .before(integrator_after_system),
-            friction_force_system
-                .after(integrator_before_system)
-                .after(gravity_system)
-                .after(player_force_system)
-                .before(integrator_after_system),
-            integrator_after_system,
-            collision_system.after(integrator_after_system),
-            friction_impulse_system.after(collision_system),
-        ));
+        // Chaining should ensure the correct execution order
+        app.add_systems(
+            FixedUpdate,
+            (
+                player_impulse_system,
+                integrator_before_system,
+                (gravity_system, player_force_system).after(integrator_before_system),
+                friction_force_system,
+                integrator_after_system,
+                collision_system,
+                friction_impulse_system,
+            )
+                .chain(),
+        );
     }
 }
 
@@ -105,7 +113,7 @@ impl FidgetSpinner {
         Self {
             radius,
             bump_size: radius / 16.0,
-            ..default()
+            ..Default::default()
         }
     }
 }
@@ -118,7 +126,6 @@ impl From<FidgetSpinner> for Mesh {
             bumps,
             vertices,
         } = shape;
-
         let mut positions = Vec::with_capacity(vertices + 1);
         let mut normals = Vec::with_capacity(vertices + 1);
 
@@ -130,7 +137,6 @@ impl From<FidgetSpinner> for Mesh {
             let theta = i as f32 * step;
             let (sin, cos) = theta.sin_cos();
             let offset = bump_size * f32::cos(bumps as f32 * theta);
-
             positions.push([cos * (radius + offset), sin * (radius + offset), 0.0]);
             normals.push([0.0, 0.0, 1.0]);
         }
@@ -141,10 +147,13 @@ impl From<FidgetSpinner> for Mesh {
         }
         indices.extend_from_slice(&[0, vertices as u32, 1]);
 
-        let mut mesh = Mesh::new(bevy::render::mesh::PrimitiveTopology::TriangleList);
+        let mut mesh = Mesh::new(
+            PrimitiveTopology::TriangleList,
+            RenderAssetUsages::MAIN_WORLD,
+        );
         mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
         mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-        mesh.set_indices(Some(bevy::render::mesh::Indices::U32(indices)));
+        mesh.insert_indices(Indices::U32(indices));
         mesh
     }
 }
@@ -154,17 +163,13 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    // 2D orthographic camera
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2d);
 
     // Player ball
     commands.spawn((
-        ColorMesh2dBundle {
-            mesh: meshes.add(FidgetSpinner::new(PLAYER_RADIUS).into()).into(),
-            material: materials.add(Color::BLUE.into()),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            ..default()
-        },
+        Mesh2d(meshes.add(FidgetSpinner::new(PLAYER_RADIUS))),
+        MeshMaterial2d(materials.add(Color::from(Srgba::BLUE))),
+        Transform::from_xyz(0.0, 0.0, 0.0),
         PhysObj {
             mass: 10.0,
             vel: Vec2::ZERO,
@@ -193,52 +198,51 @@ fn setup(
 
 fn gravity_system(mut query: Query<(&mut PhysObj, &Gravity)>) {
     for (mut phys_obj, gravity) in &mut query {
-        let a = Vec2::NEG_Y * gravity.0;
-        phys_obj.acc += a;
+        phys_obj.acc += Vec2::NEG_Y * gravity.0;
     }
 }
 
 fn player_impulse_system(
     mut commands: Commands,
-    input: Res<Input<KeyCode>>,
-    mut query: Query<(Entity, &Player, &mut PhysObj, &Collider)>,
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(Entity, &Player, &mut PhysObj, &mut Collider)>,
 ) {
-    let (
-        entity,
-        player,
-        mut phys_obj,
-        Collider::Ball {
+    for (entity, player, mut phys_obj, mut collider) in &mut query {
+        let Collider::Ball {
             touching_ground, ..
-        },
-    ) = query.single_mut();
+        } = &mut *collider;
 
-    if input.pressed(KeyCode::Space) && *touching_ground {
-        let dv = Vec2::Y * player.jump_impulse / phys_obj.mass;
-        phys_obj.vel += dv;
-    }
+        if input.pressed(KeyCode::Space) && *touching_ground {
+            let dv = Vec2::Y * player.jump_impulse / phys_obj.mass;
+            phys_obj.vel += dv;
+        }
 
-    if input.just_pressed(KeyCode::K) {
-        commands.entity(entity).remove::<Gravity>();
-    }
-    if input.just_released(KeyCode::K) {
-        commands.entity(entity).insert(Gravity::default());
+        if input.just_pressed(KeyCode::KeyK) {
+            commands.entity(entity).remove::<Gravity>();
+        }
+        if input.just_released(KeyCode::KeyK) {
+            commands.entity(entity).insert(Gravity::default());
+        }
     }
 }
 
-fn player_force_system(input: Res<Input<KeyCode>>, mut query: Query<(&Player, &mut PhysObj)>) {
-    let (player, mut phys_obj) = query.single_mut();
-
-    if input.pressed(KeyCode::A) {
-        phys_obj.angular_acc += player.torque / phys_obj.moment_of_inertia;
-    }
-    if input.pressed(KeyCode::D) {
-        phys_obj.angular_acc -= player.torque / phys_obj.moment_of_inertia;
+fn player_force_system(
+    input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&Player, &mut PhysObj)>,
+) {
+    for (player, mut phys_obj) in &mut query {
+        if input.pressed(KeyCode::KeyA) {
+            phys_obj.angular_acc += player.torque / phys_obj.moment_of_inertia;
+        }
+        if input.pressed(KeyCode::KeyD) {
+            phys_obj.angular_acc -= player.torque / phys_obj.moment_of_inertia;
+        }
     }
 }
 
 // The part of the integrator that runs before applying forces
 fn integrator_before_system(time: Res<Time>, mut query: Query<(&mut Transform, &mut PhysObj)>) {
-    let dt = time.delta_seconds();
+    let dt = time.delta_secs();
     for (mut transform, mut phys_obj) in &mut query {
         integrate_before(dt, &mut transform, &mut phys_obj);
     }
@@ -246,50 +250,38 @@ fn integrator_before_system(time: Res<Time>, mut query: Query<(&mut Transform, &
 
 // The part of the integrator that runs after applying forces
 fn integrator_after_system(time: Res<Time>, mut query: Query<&mut PhysObj>) {
-    let dt = time.delta_seconds();
+    let dt = time.delta_secs();
     for mut phys_obj in &mut query {
         integrate_after(dt, &mut phys_obj);
     }
 }
 
-// The part of the integrator that runs before applying forces
-fn integrate_before(dt: f32, transform: &mut Mut<Transform>, phys_obj: &mut Mut<PhysObj>) {
-    let dv = 0.5 * phys_obj.acc * dt;
-    phys_obj.vel += dv;
-    let dx = phys_obj.vel * dt;
-    transform.translation += dx.extend(0.0);
+fn integrate_before(dt: f32, transform: &mut Transform, phys_obj: &mut PhysObj) {
+    phys_obj.vel += 0.5 * phys_obj.acc * dt;
+    transform.translation += (phys_obj.vel * dt).extend(0.0);
     phys_obj.acc_prev = phys_obj.acc;
     // Functions that calculate acceleration simply add to it so it must be reset every iteration.
     phys_obj.acc = Vec2::ZERO;
 
-    let dav = 0.5 * phys_obj.angular_acc * dt;
-    phys_obj.angular_vel += dav;
-    let angle = phys_obj.angular_vel * dt;
-    transform.rotate_z(angle);
+    phys_obj.angular_vel += 0.5 * phys_obj.angular_acc * dt;
+    transform.rotate_z(phys_obj.angular_vel * dt);
     phys_obj.angular_acc_prev = phys_obj.angular_acc;
-    // Functions that calculate acceleration simply add to it so it must be reset every iteration.
     phys_obj.angular_acc = 0.0;
 }
 
-// The part of the integrator that runs after applying forces
-fn integrate_after(dt: f32, phys_obj: &mut Mut<PhysObj>) {
-    let dv = 0.5 * phys_obj.acc * dt;
-    phys_obj.vel += dv;
-
-    let dav = 0.5 * phys_obj.angular_acc * dt;
-    phys_obj.angular_vel += dav;
+fn integrate_after(dt: f32, phys_obj: &mut PhysObj) {
+    phys_obj.vel += 0.5 * phys_obj.acc * dt;
+    phys_obj.angular_vel += 0.5 * phys_obj.angular_acc * dt;
 }
 
 // Integrator for when acceleration is assumed constant (used in collision resolving)
-fn integrate_simple(dt: f32, transform: &mut Mut<Transform>, phys_obj: &mut Mut<PhysObj>) {
+fn integrate_simple(dt: f32, transform: &mut Transform, phys_obj: &mut PhysObj) {
     let dv = phys_obj.acc * dt;
-    let dx = (phys_obj.vel + 0.5 * dv) * dt;
-    transform.translation += dx.extend(0.0);
+    transform.translation += ((phys_obj.vel + 0.5 * dv) * dt).extend(0.0);
     phys_obj.vel += dv;
 
     let dav = phys_obj.angular_acc * dt;
-    let angle = (phys_obj.angular_vel + 0.5 * dav) * dt;
-    transform.rotate_z(angle);
+    transform.rotate_z((phys_obj.angular_vel + 0.5 * dav) * dt);
     phys_obj.angular_vel += dav;
 }
 
@@ -297,7 +289,7 @@ fn collision_system(
     time: Res<Time>,
     mut query: Query<(&mut Transform, &mut PhysObj, &mut Collider)>,
 ) {
-    let dt = time.delta_seconds();
+    let dt = time.delta_secs();
     for (mut transform, mut phys_obj, mut collider) in &mut query {
         match *collider {
             Collider::Ball {
@@ -307,7 +299,7 @@ fn collision_system(
             } => {
                 if transform.translation.y - radius <= FLOOR_Y {
                     while resolve_collision(dt, &mut transform, &mut phys_obj, &mut collider) {}
-                } else if *touching_ground {
+                } else {
                     *touching_ground = false;
                 }
             }
@@ -317,24 +309,24 @@ fn collision_system(
 
 fn resolve_collision(
     dt: f32,
-    transform: &mut Mut<Transform>,
-    phys_obj: &mut Mut<PhysObj>,
-    collider: &mut Mut<Collider>,
+    transform: &mut Transform,
+    phys_obj: &mut PhysObj,
+    collider: &mut Collider,
 ) -> bool {
-    match **collider {
+    match collider {
         Collider::Ball {
             radius,
             touching_ground: true,
             ..
         } => {
-            transform.translation.y = FLOOR_Y + radius;
+            transform.translation.y = FLOOR_Y + *radius;
             phys_obj.vel.y = 0.0;
             false
         }
         Collider::Ball {
             radius,
             coef_of_restitution,
-            ref mut touching_ground,
+            touching_ground,
             kinetic_friction,
             ..
         } => {
@@ -343,9 +335,9 @@ fn resolve_collision(
                 dt,
                 transform,
                 phys_obj,
-                radius,
-                coef_of_restitution,
-                kinetic_friction,
+                *radius,
+                *coef_of_restitution,
+                *kinetic_friction,
             )
         }
     }
@@ -353,40 +345,41 @@ fn resolve_collision(
 
 fn bounce(
     dt: f32,
-    transform: &mut Mut<Transform>,
-    phys_obj: &mut Mut<PhysObj>,
+    transform: &mut Transform,
+    phys_obj: &mut PhysObj,
     radius: f32,
     coef_of_restitution: f32,
     kinetic_friction: f32,
 ) -> bool {
-    let (s, v, a) = (
+    let collision_dt = calculate_collision_dt(
         (transform.translation.y - radius) - FLOOR_Y,
         phys_obj.vel.y,
         phys_obj.acc.y,
     );
-    let collision_dt = calculate_collision_dt(s, v, a);
 
     if collision_dt > 0.5 * dt || collision_dt.is_nan() {
         integrate_simple(-0.5 * dt, transform, phys_obj);
+        std::mem::swap(&mut phys_obj.acc, &mut phys_obj.acc_prev);
 
-        let (s, v, a) = (
+        let collision_dt2 = calculate_collision_dt(
             (transform.translation.y - radius) - FLOOR_Y,
             phys_obj.vel.y,
-            phys_obj.acc_prev.y,
+            phys_obj.acc.y,
         );
-        let collision_dt2 = calculate_collision_dt(s, v, a);
         assert!(collision_dt2 >= 0.0);
 
-        (phys_obj.acc, phys_obj.acc_prev) = (phys_obj.acc_prev, phys_obj.acc); // Don't try this at home (bad code)
         integrate_simple(-collision_dt2, transform, phys_obj);
-
-        let normal_impulse = -phys_obj.vel.y * (1.0 + coef_of_restitution);
-        apply_friction_impulse(phys_obj, radius, normal_impulse, kinetic_friction, 0.0);
+        apply_friction_impulse(
+            phys_obj,
+            radius,
+            -phys_obj.vel.y * (1.0 + coef_of_restitution),
+            kinetic_friction,
+            0.0,
+        );
         phys_obj.vel.y *= -coef_of_restitution;
-
         integrate_simple(collision_dt2, transform, phys_obj);
-        (phys_obj.acc, phys_obj.acc_prev) = (phys_obj.acc_prev, phys_obj.acc); // Don't try this at home (bad code)
 
+        std::mem::swap(&mut phys_obj.acc, &mut phys_obj.acc_prev);
         integrate_simple(0.5 * dt, transform, phys_obj);
     } else {
         assert!(collision_dt >= 0.0);
@@ -398,23 +391,24 @@ fn bounce(
 
         integrate_simple(collision_dt, transform, phys_obj);
     }
-    false // TODO: Calculate time until bouncing stops and proceed as follows:
-          //    - If that time is less than the time step, approximate behavior that results in
-          //        the ball laying/sliding on the ground at the end of the frame.
-          //    - Otherwise, bounce once and return whether another bounce will happen during the frame.
-          // Written out this seems like a bad way to do it... That's a problem for another day.
+    false
+    // TODO: Calculate time until bouncing stops and proceed as follows:
+    //    - If that time is less than the time step, approximate behavior that results in
+    //        the ball laying/sliding on the ground at the end of the frame.
+    //    - Otherwise, bounce once and return whether another bounce will happen during the frame.
+    // Written out this seems like a bad way to do it... That's a problem for another day.
 }
 
 fn calculate_collision_dt(s: f32, v: f32, a: f32) -> f32 {
-    if a == 0.0 {
+    if a.abs() < f32::EPSILON {
         s / v
     } else {
-        (v - f32::sqrt(v.powi(2) - 2.0 * a * s).copysign(v)) / a
+        (v - (v.powi(2) - 2.0 * a * s).sqrt().copysign(v)) / a
     }
 }
 
 fn apply_friction_impulse(
-    phys_obj: &mut Mut<PhysObj>,
+    phys_obj: &mut PhysObj,
     radius: f32,
     normal_impulse: f32,
     kinetic_friction: f32,
@@ -432,7 +426,7 @@ fn apply_friction_impulse(
 }
 
 fn friction_impulse_system(time: Res<Time>, mut query: Query<(&mut PhysObj, &Collider)>) {
-    let dt = time.delta_seconds();
+    let dt = time.delta_secs();
     for (mut phys_obj, collider) in &mut query {
         if let Collider::Ball {
             radius,
@@ -443,7 +437,7 @@ fn friction_impulse_system(time: Res<Time>, mut query: Query<(&mut PhysObj, &Col
             ..
         } = *collider
         {
-            if phys_obj.vel.y == 0.0 {
+            if phys_obj.vel.y.abs() < 0.1 {
                 let normal_impulse = -(phys_obj.acc.y + phys_obj.acc_prev.y) * 0.5 * dt;
                 let applied_friction = (friction_acc + friction_acc_prev) * 0.5 * dt;
                 apply_friction_impulse(
@@ -470,35 +464,16 @@ fn friction_force_system(mut query: Query<(&mut PhysObj, &mut Collider)>) {
         } = *collider
         {
             let normal_force = -phys_obj.acc.y;
-            apply_friction_force(
-                &mut phys_obj,
-                radius,
-                normal_force,
-                kinetic_friction,
-                friction_acc,
-                friction_acc_prev,
-            );
+            let rel_acc = phys_obj.acc.x + phys_obj.angular_acc * radius;
+            let max_f = normal_force * kinetic_friction;
+            let stop_f = phys_obj.moment_of_inertia * rel_acc.abs()
+                / (phys_obj.mass * radius.powi(2) + phys_obj.moment_of_inertia);
+            let force = f32::min(max_f, stop_f).copysign(-rel_acc);
+
+            *friction_acc_prev = *friction_acc;
+            *friction_acc = force;
+            phys_obj.acc.x += force;
+            phys_obj.angular_acc += force * phys_obj.mass * radius / phys_obj.moment_of_inertia;
         }
     }
-}
-
-fn apply_friction_force(
-    phys_obj: &mut Mut<PhysObj>,
-    radius: f32,
-    normal_force: f32,
-    kinetic_friction: f32,
-    friction_acc: &mut f32,
-    friction_acc_prev: &mut f32,
-) {
-    let relative_acceleration = phys_obj.acc.x + phys_obj.angular_acc * radius;
-    let max_force = normal_force * kinetic_friction;
-    let stopping_force = phys_obj.moment_of_inertia * relative_acceleration.abs()
-        / (phys_obj.mass * radius.powi(2) + phys_obj.moment_of_inertia);
-    let force = f32::min(max_force, stopping_force).copysign(-relative_acceleration);
-
-    *friction_acc_prev = *friction_acc;
-    *friction_acc = force;
-
-    phys_obj.acc.x += force;
-    phys_obj.angular_acc += force * phys_obj.mass * radius / phys_obj.moment_of_inertia;
 }
